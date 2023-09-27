2042c21c0f377ff26a3cb8930321dd448d78b509
jozef1371 <usefnajafpoor@gmail.com>
2023-09-27 19:22:37 +0330

    Update MainWindow.cs

--- Graphics/Point/MainWindow.cs
+++ Graphics/Point/MainWindow.cs
@@ -1,9 +1,9 @@
 ﻿// // Copyright (c) Microsoft. All rights reserved.
 // // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Media;
+//using System.Windows;
+//using System.Windows.Controls;
+//using System.Windows.Media;
 
 namespace Point
 {
